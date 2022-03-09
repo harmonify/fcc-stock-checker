2a9a14bf6c28ac63872ef7cd5c99993479fa6aa6
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-09 22:29:37 +0700

    add functional test barebones

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,72 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  this.timeout(5000);
 
+  const baseUrl = "/api/stock-prices";
+
+  test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get(baseUrl)
+      .query()
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.fail("NotImplemented");
+        done();
+      });
+  });
+
+  test("Viewing one stock and liking it: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get(baseUrl)
+      .query()
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.fail("NotImplemented");
+        done();
+      });
+  });
+
+  test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get(baseUrl)
+      .query()
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.fail("NotImplemented");
+        done();
+      });
+  });
+
+  test("Viewing two stocks: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get(baseUrl)
+      .query()
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.fail("NotImplemented");
+        done();
+      });
+  });
+
+  test("Viewing two stocks and liking them: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get(baseUrl)
+      .query()
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.fail("NotImplemented");
+        done();
+      });
+  });
 });
