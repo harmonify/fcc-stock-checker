1892760d7a7df5b5bdef0bb1e40e072c9a60ebae
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-11 02:07:13 +0700

    change helmet v5 to v3

--- server.js
+++ server.js
@@ -24,7 +24,15 @@ const { errorHandler, loggerDev, notFoundHandler } = require("./middlewares");
 const app = express();
 
 app.use(loggerDev);
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    // configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+    },
+  },
+}));
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cors({ origin: "*" })); //For FCC testing purposes only
 app.use(bodyParser.json());
